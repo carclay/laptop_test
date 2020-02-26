6d57d80da202bbcd6f192eba246adf2f48a91ede
volga <vtikunov@yandex.ru>
2020-02-26 13:56:55 +0300

    [feat] Изменение требований к методу getId ModelInterface

--- test/Model/ModelInterfaceTest.php
+++ test/Model/ModelInterfaceTest.php
@@ -21,8 +21,23 @@ final class ModelInterfaceTest extends TestCase
         $this->assertTrue($this->class->isInterface());
     }
 
-    public function test_It_has_getId_method(): void
+    /**
+     * @throws \ReflectionException
+     */
+    public function test_It_has_getId_method_with_return_type_is_not_declared(): void
     {
-        $this->assertTrue($this->class->hasMethod('getId'));
+        $method = 'getId';
+
+        $this->assertTrue($this->class->hasMethod($method));
+
+        /**
+         * @var \ReflectionNamedType|null
+         */
+        $returnType = $this->class->getMethod($method)->getReturnType();
+
+        $this->assertTrue(
+            null === $returnType,
+            'Method returns invalid type'
+        );
     }
 }
